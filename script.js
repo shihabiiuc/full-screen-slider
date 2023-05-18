9afedb597823e96817b021b305e43ea46ae0afb4
Shihab <hello@shihabiiuc.com>
2023-05-19 01:34:20 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// HEADER NAVIGATION
+const hamburger = document.getElementsByClassName("hamburger")[0]
+const mobileNavs = document.getElementsByClassName("nav-links")[0]
+
+hamburger.addEventListener("click", () => {
+  mobileNavs.classList.toggle("active")
+})
+
+// SLIDER
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex";
+}
